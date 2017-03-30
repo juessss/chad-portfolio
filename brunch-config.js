6b857ffdb618c46b433757bf90240be03ce72b39
Gemma D Lou <gblackuk@googlemail.com>
2017-03-30 06:27:33 +0000

    Brunch To Replace Webpack

---
+++ brunch-config.js
@@ -0,0 +1,35 @@
+// See http://brunch.io for documentation.
+exports.files = {
+  javascripts: {
+    joinTo: {
+      'vendor.js': /^(?!app)/, // Files that are not in `app` dir.
+      'app.js': /^app/
+    }
+  },
+  stylesheets: {joinTo: 'app.css'}
+};
+
+exports.plugins = {
+  babel: {presets: ['latest']}
+};
+
+exports.server = {
+  port: 8080,
+  stripSlashes: true,
+  hostname: '0.0.0.0'
+}
+
+exports.plugins = {
+  postcss: {
+    processors: [
+      require('autoprefixer')(['last 8 versions']),
+      require('csswring')()
+    ]
+  },
+  babel: {
+    presets: ['env', 'react'],
+    ignore: [
+      /^node_modules/
+    ]
+  }
+}
