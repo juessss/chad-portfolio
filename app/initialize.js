b022237e2fc942c1ee8556c8f8155d0fc9d25346
Gemma D Lou <gblackuk@googlemail.com>
2017-03-30 06:40:26 +0000

    React Setup

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // do your setup here
-  console.log('Initialized app');
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello, world</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
