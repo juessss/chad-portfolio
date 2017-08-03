dda6d81e266baeca17bf9de74a6b5c03f3b54b05
Gemma D Lou <gblackuk@googlemail.com>
2017-08-03 00:28:15 +0000

    Slider Added

--- app/src/Framework/ReactRoutes/Home/project.js
+++ app/src/Framework/ReactRoutes/Home/project.js
@@ -1,179 +1,191 @@
 import React, { Component } from 'react';
 import Hero from '../../Components/Hero/hero.js';
 import ImageGallery from '../../Components/Image-Gallery/imagegallery.js';
+import Slider from 'jgb-slider';
 
-const Project = () => {
-    return (
-      <div>
-
-        <Hero></Hero>
-
-        <div className="prj-summary">
-          <div className="prj-summary__lead">
-            <span className="prj-summary__lead-text">
-                Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur
-            </span>
-          </div>
-          <ul className="prj-summary__meta">
-            <li className="prj-summary__meta-item">
-              <h2 className="prj-summary__meta-title">Awards & Titles</h2>
-              <p className="prj-summary__meta-text">ABC, EFG, LMNOP</p>
-            </li>
-
-            <li className="prj-summary__meta-item">
-              <h2 className="prj-summary__meta-title">Shoutouts</h2>
-              <p className="prj-summary__meta-text">Tom, John and Luke</p>
-            </li>
-
-            <li className="prj-summary__meta-item">
-              <h2 className="prj-summary__meta-title">Visit Client</h2>
-              <p className="prj-summary__meta-text">www.abc.com</p>
-            </li>
-          </ul>
-        </div>
-
-        <div className="prj-minimal-info">
-          <div className="prj-minimal-info__text">
-            <div className="prj-minimal-info__text-title">
-                Contextual
+class Project extends Component {
+    
+    componentDidMount() {
+      new Slider({ selector: '.slider' });
+    }
+    
+    shouldComponentUpdate() {
+      return false;
+    }
+  
+    render() {
+      return (
+        <div>
+  
+          <Hero></Hero>
+  
+          <div className="prj-summary">
+            <div className="prj-summary__lead">
+              <span className="prj-summary__lead-text">
+                  Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur
+              </span>
             </div>
-            <div className="prj-minimal-info__text-content">
-                Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur
-            </div>
-          </div>
-          <div className="prj-minimal-info__main-image-wrapper">
-            <img className="prj-minimal-info__image" src="http://unsplash.it/800/800?image=937" />
-          </div>
-          <div className="prj-minimal-info__small-image-wrapper">
-            <img className="prj-minimal-info__image" src="http://unsplash.it/800/800?image=237" />
-          </div>
-        </div>
-
-        <div className="prj-typography">
-          <div className="prj-typography__item">
-            <h3 className="prj-typography__item-header">
-              Heading
-            </h3>
-            <span className="prj-typography__item-sub-header">
-              Roboto
-            </span>
-            <div className="prj-typography__item-example">
-              Image goes here
-            </div>
-          </div>
-          <div className="prj-typography__item">
-            <h3 className="prj-typography__item-header">
-              Heading
-            </h3>
-            <span className="prj-typography__item-sub-header">
-              Roboto
-            </span>
-            <div className="prj-typography__item-example">
-              Image goes here
-            </div>
-          </div>
-          <div className="prj-typography__item">
-            <h3 className="prj-typography__item-header">
-              Heading
-            </h3>
-            <span className="prj-typography__item-sub-header">
-              Roboto
-            </span>
-            <div className="prj-typography__item-example">
-              Image goes here
-            </div>
-          </div>
-        </div>
-
-        <div className="palette-section">
-          <div className="palette-section__blurb">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-          </div>
-          <div className="palette-section__color-board">
-            <ul className="color-board">
-              <li className="color-board__item">
-                <span className="color-board__item-name">Super green</span>
-                <span className="color-board__item-color">#123131</span>
+            <ul className="prj-summary__meta">
+              <li className="prj-summary__meta-item">
+                <h2 className="prj-summary__meta-title">Awards & Titles</h2>
+                <p className="prj-summary__meta-text">ABC, EFG, LMNOP</p>
               </li>
-              <li className="color-board__item">
-                <span className="color-board__item-name">Super green</span>
-                <span className="color-board__item-color">#123131</span>
+  
+              <li className="prj-summary__meta-item">
+                <h2 className="prj-summary__meta-title">Shoutouts</h2>
+                <p className="prj-summary__meta-text">Tom, John and Luke</p>
               </li>
-              <li className="color-board__item">
-                <span className="color-board__item-name">Super green</span>
-                <span className="color-board__item-color">#123131</span>
-              </li>
-              <li className="color-board__item">
-                <span className="color-board__item-name">Super green</span>
-                <span className="color-board__item-color">#123131</span>
-              </li>
-              <li className="color-board__item">
-                <span className="color-board__item-name">Super green</span>
-                <span className="color-board__item-color">#123131</span>
-              </li>
-              <li className="color-board__item">
-                <span className="color-board__item-name">Super green</span>
-                <span className="color-board__item-color">#123131</span>
+  
+              <li className="prj-summary__meta-item">
+                <h2 className="prj-summary__meta-title">Visit Client</h2>
+                <p className="prj-summary__meta-text">www.abc.com</p>
               </li>
             </ul>
           </div>
-        </div>
-        
-        <div className="prj-dual-hero">
-          <div className="prj-dual-hero__image-wrapper">
-            <img className="prj-dual-hero__image" src="//unsplash.it/800/800?image=123" />
-          </div>
-          <div className="prj-dual-hero__image-wrapper">
-            <img className="prj-dual-hero__image" src="//unsplash.it/800/800?image=234" />
-          </div>
-        </div>
-
-        <Hero></Hero>
-
-        <div className="prj-minimal-info">
-          <div className="prj-minimal-info__text">
-            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur
-          </div>
-          <div className="prj-minimal-info__main-image-wrapper">
-            <img className="prj-minimal-info__image" src="http://unsplash.it/800/800?image=937" />
-          </div>
-          <div className="prj-minimal-info__small-image-wrapper">
-            <img className="prj-minimal-info__image" src="http://unsplash.it/800/800?image=937" />
-          </div>
-        </div>
-
-        <ImageGallery></ImageGallery>
-        
-        <div className="prj-slider">
-          <div className="prj-slider__blurb">
-            
-          </div>
-          <div className="prj-slider__slide">
-            <div class="slider">
-              <ol class="slider__slider">
-                <li class="slider__slide">
-                    <img src="http://unsplash.it/1200/500" />
-                </li>
-                <li class="slider__slide">
-                    <img src="http://unsplash.it/1200/500?image=123" />
-                </li>
-                <li class="slider__slide">
-                    <img src="http://unsplash.it/1200/500?image=173" />
-                </li>
-                <li class="slider__slide">
-                    <img src="http://unsplash.it/1200/500?image=143" />
-                </li>
-                <li class="slider__slide">
-                    <img src="http://unsplash.it/1200/500?image=423" />
-                </li>
-              </ol>
+  
+          <div className="prj-minimal-info">
+            <div className="prj-minimal-info__text">
+              <div className="prj-minimal-info__text-title">
+                  Contextual
+              </div>
+              <div className="prj-minimal-info__text-content">
+                  Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur
+              </div>
+            </div>
+            <div className="prj-minimal-info__main-image-wrapper">
+              <img className="prj-minimal-info__image" src="http://unsplash.it/800/800?image=937" />
+            </div>
+            <div className="prj-minimal-info__small-image-wrapper">
+              <img className="prj-minimal-info__image" src="http://unsplash.it/800/800?image=237" />
             </div>
           </div>
+  
+          <div className="prj-typography">
+            <div className="prj-typography__item">
+              <h3 className="prj-typography__item-header">
+                Heading
+              </h3>
+              <span className="prj-typography__item-sub-header">
+                Roboto
+              </span>
+              <div className="prj-typography__item-example">
+                Image goes here
+              </div>
+            </div>
+            <div className="prj-typography__item">
+              <h3 className="prj-typography__item-header">
+                Heading
+              </h3>
+              <span className="prj-typography__item-sub-header">
+                Roboto
+              </span>
+              <div className="prj-typography__item-example">
+                Image goes here
+              </div>
+            </div>
+            <div className="prj-typography__item">
+              <h3 className="prj-typography__item-header">
+                Heading
+              </h3>
+              <span className="prj-typography__item-sub-header">
+                Roboto
+              </span>
+              <div className="prj-typography__item-example">
+                Image goes here
+              </div>
+            </div>
+          </div>
+  
+          <div className="palette-section">
+            <div className="palette-section__blurb">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </div>
+            <div className="palette-section__color-board">
+              <ul className="color-board">
+                <li className="color-board__item">
+                  <span className="color-board__item-name">Super green</span>
+                  <span className="color-board__item-color">#123131</span>
+                </li>
+                <li className="color-board__item">
+                  <span className="color-board__item-name">Super green</span>
+                  <span className="color-board__item-color">#123131</span>
+                </li>
+                <li className="color-board__item">
+                  <span className="color-board__item-name">Super green</span>
+                  <span className="color-board__item-color">#123131</span>
+                </li>
+                <li className="color-board__item">
+                  <span className="color-board__item-name">Super green</span>
+                  <span className="color-board__item-color">#123131</span>
+                </li>
+                <li className="color-board__item">
+                  <span className="color-board__item-name">Super green</span>
+                  <span className="color-board__item-color">#123131</span>
+                </li>
+                <li className="color-board__item">
+                  <span className="color-board__item-name">Super green</span>
+                  <span className="color-board__item-color">#123131</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          
+          <div className="prj-dual-hero">
+            <div className="prj-dual-hero__image-wrapper">
+              <img className="prj-dual-hero__image" src="//unsplash.it/800/800?image=123" />
+            </div>
+            <div className="prj-dual-hero__image-wrapper">
+              <img className="prj-dual-hero__image" src="//unsplash.it/800/800?image=234" />
+            </div>
+          </div>
+  
+          <Hero></Hero>
+  
+          <div className="prj-minimal-info">
+            <div className="prj-minimal-info__text">
+              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur
+            </div>
+            <div className="prj-minimal-info__main-image-wrapper">
+              <img className="prj-minimal-info__image" src="http://unsplash.it/800/800?image=937" />
+            </div>
+            <div className="prj-minimal-info__small-image-wrapper">
+              <img className="prj-minimal-info__image" src="http://unsplash.it/800/800?image=937" />
+            </div>
+          </div>
+  
+          <ImageGallery></ImageGallery>
+          
+          <div className="prj-slider">
+            <div className="prj-slider__blurb">
+              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur
+            </div>
+            <div className="prj-slider__slide">
+              <div className="slider">
+                <ol className="slider__slider">
+                  <li className="slider__slide">
+                      <img src="http://unsplash.it/1200/500" />
+                  </li>
+                  <li className="slider__slide">
+                      <img src="http://unsplash.it/1200/500?image=123" />
+                  </li>
+                  <li className="slider__slide">
+                      <img src="http://unsplash.it/1200/500?image=173" />
+                  </li>
+                  <li className="slider__slide">
+                      <img src="http://unsplash.it/1200/500?image=143" />
+                  </li>
+                  <li className="slider__slide">
+                      <img src="http://unsplash.it/1200/500?image=423" />
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
+  
+          <Hero></Hero>
         </div>
-
-        <Hero></Hero>
-      </div>
-    );
+      );
+    }
 }
 
 export default Project;
